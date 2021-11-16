3477d7db92aa07b168ba973bc8190a82cf260a42
Nikhil Repale <repalena19.comp@coep.ac.in>
2021-11-16 10:58:21 +0530

    Prime and Power added

--- operations.py
+++ operations.py
@@ -1,5 +1,4 @@
-
-
+from math import sqrt
 
 # Method that takes a binary string and converts the string into a decimal number.
 def binToDec(binaryNumber):
@@ -23,3 +22,39 @@ def binToDec(binaryNumber):
 
     finally:
         return num
+
+# checks whether given number is prime or not
+
+def isPrime(n):
+
+    try:
+        m = int(n)
+        prime_flag = True
+        if(m > 1):
+            uppnum = int(sqrt(m))+1
+            print(uppnum)
+            for i in range(2,uppnum):
+                if(m%i== 0):
+                    prime_flag = False
+                    break
+            
+    except Exception as ex:
+        print("Unable to check for prime number", ex)
+
+    finally:
+        return prime_flag
+
+
+
+
+def power(N, P):
+    try:
+                
+        if P == 0:
+            return 1
+        elif P == 1:
+            return N
+        else:
+            return (N*power(N, P-1))
+    except Exception as ex:
+        print("unable to calculate power", ex)
