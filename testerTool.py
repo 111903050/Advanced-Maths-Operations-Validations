df55c050883feb26839543fe969166c2d309158b
Nikhil Repale <repalena19.comp@coep.ac.in>
2021-11-16 11:25:36 +0530

    test cases added

--- testerTool.py
+++ testerTool.py
@@ -61,4 +61,48 @@ class test_02(unittest.TestCase):
 
         finally:
             return
+            
+            
+# test_03 for testing invalid input of only characters
+class test_03(unittest.TestCase):
+    def runTest(self):
+
+        try:
+            response = tester("1", "abcdeghlloklmmsiml")
+            expected = 2
+            msg = f"The answer returned is not as per the expectation, it is {response}"
+            self.assertEqual(response, expected, msg)
+
+            print("\nTEST-03 PASS : The returned answer is correct as expected", response)
+            print(
+                "\n========================================================================\n")
+        except Exception as ex:
+            print('\n\nTEST-03 FAIL', ex)
+            print(
+                "\n========================================================================\n")
+
+        finally:
+            return
+            
+            
+# test_04 for testing invalid input of constiting values other than 1 an 0
+class test_04(unittest.TestCase):
+    def runTest(self):
+
+        try:
+            response = tester("1", "11111111112223444444444555")
+            expected = 2
+            msg = f"The answer returned is not as per the expectation, it is {response}"
+            self.assertEqual(response, expected, msg)
+
+            print("\nTEST-04 PASS : The returned answer is correct as expected", response)
+            print(
+                "\n========================================================================\n")
+        except Exception as ex:
+            print('\n\nTEST-04 FAIL', ex)
+            print(
+                "\n========================================================================\n")
+
+        finally:
+            return
 
